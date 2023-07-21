9059e34dc2591b5b39687a2ed3f9cdd97b027a78
Andrew Gabra <123777400+agabra23@users.noreply.github.com>
2023-07-21 13:41:48 -0400

    Layout BasicInfo component

--- src/components/BasicInfo.jsx
+++ src/components/BasicInfo.jsx
@@ -6,6 +6,15 @@ export default function BasicInfo() {
 
         <label htmlFor="fullName">Full Name</label>
         <input type="text" id="fullName" />
+
+        <label htmlFor="email">Email</label>
+        <input type="email" id="email" />
+
+        <label htmlFor="phoneNumber">Phone Number</label>
+        <input type="phone" id="phoneNumber" />
+
+        <label htmlFor="website">Website or LinkedIn URL</label>
+        <input type="text" id="website" />
       </fieldset>
     </div>
   );
