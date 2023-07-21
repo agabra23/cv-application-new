678c4ad9559eb60fb2f50df4d4415b79bdab52ad
Andrew Gabra <123777400+agabra23@users.noreply.github.com>
2023-07-21 14:36:06 -0400

    Finish Skill component implementation with SkillsOverview

--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -1,18 +1,51 @@
 import SkillsOverview from "./SkillsOverview";
+import { useState } from "react";
 
 export default function Skills() {
+  const [skillsList, setSkillsList] = useState([]);
+  const [skillValue, setSkillValue] = useState("");
+
+  const addSkill = () => {
+    setSkillsList([...skillsList, skillValue]);
+    setSkillValue("");
+  };
+
+  const deleteItem = (index) => {
+    const tempList = [...skillsList];
+    tempList.splice(index, 1);
+    setSkillsList(tempList);
+  };
+
+  const enterHandler = (e) => {
+    if (e.key === "Enter") {
+      e.preventDefault();
+      addSkill();
+    }
+  };
+
   return (
     <fieldset className="skills-wrapper">
       <legend>Skills</legend>
 
       <label htmlFor="skillInput">Add Skills</label>
-      <input type="text" id="skillInput" />
+      <input
+        type="text"
+        id="skillInput"
+        value={skillValue}
+        onChange={(e) => setSkillValue(e.target.value)}
+        onKeyDown={enterHandler}
+      />
 
-      <button className="add-skills-btn form-btn" type="button">
+      <button
+        className="add-skills-btn form-btn"
+        type="button"
+        id="addSkillBtn"
+        onClick={addSkill}
+      >
         Add
       </button>
 
-      <SkillsOverview />
+      <SkillsOverview skills={skillsList} deleteItem={deleteItem} />
     </fieldset>
   );
 }
--- src/components/SkillsOverview.jsx
+++ src/components/SkillsOverview.jsx
@@ -1,10 +1,14 @@
-export default function SkillsOverview() {
-  return (
-    <ul className="skills-overview">
-      <li></li>
-      <li></li>
-      <li></li>
-      <li></li>
-    </ul>
-  );
+export default function SkillsOverview({ skills, deleteItem }) {
+  const skillsList = skills.map((skill, index) => {
+    return (
+      <div className="skill-list-input-item" key={skill}>
+        <li>{skill}</li>
+        <button className="delete-btn" onClick={() => deleteItem(index)}>
+          Remove
+        </button>
+      </div>
+    );
+  });
+
+  return <ul className="skills-overview">{skillsList}</ul>;
 }
